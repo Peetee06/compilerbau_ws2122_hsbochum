c89d01e9636e813b93e2de89da2024dcfcb4ed7d
Peter Trost <peter_trost93@yahoo.de>
2022-03-20 15:36:49 +0100

    implemented string representation for syntax tree

--- token.py
+++ token.py
@@ -39,3 +39,15 @@ class Token(object):
                 'def',
                 ]
 
+    def __init__(self, type_, value=None):
+        if type_ in self.TOKEN_TYPES:
+            self.type = type_
+        else:
+            self.type = None
+        self.value = value
+
+    def __repr__(self):
+        return f"{self.type}:{self.value}" if self.value else f"{self.type}"
+
+    def matches(self, type_, value):
+        return self.type == type_ and self.value == value
